352e2cd6354468175ada5f43d07615b5c34a12cf
jchinlee <jaoke.chinlee@10gen.com>
2013-06-13 17:06:38 -0400

    init commit; tcp_ex

---
+++ tcp_ex/client.rs
@@ -0,0 +1,125 @@
+extern mod std;
+extern mod core;
+
+use std::net::*;
+use ip = net_ip;
+use tcp = net_tcp;
+use std::uv;
+
+/**
+ * Parse user input string and transform into bytes.
+ *
+ * # Arguments
+ *
+ * * `input` - input as a string
+ *
+ * # Returns
+ *
+ * byte representation of input for transmitting over socket
+ */
+fn process_input(input : ~str) -> ~[u8] {
+    // no parsing, actually ; just direct conversion
+    str::to_bytes(input)
+}
+
+/**
+ * Actually send request in byte form to server and get result.
+ *
+ * # Arguments
+ *
+ * * `sock` - the socket over which to send the request
+ * * `request` - the request, in byte form
+ * * `port` - the port over which responses will be sent
+ *
+ * # Returns
+ *
+ * result of write
+ */
+fn send_request(sock : &tcp::TcpSocket, request : ~[u8], port : @Port<result::Result<~[u8], tcp::TcpErrData>>)
+            -> result::Result<(), tcp::TcpErrData> {
+    // send request to server (asynchronously; "future")
+    let mut write_result_future = sock.write_future(copy request);
+    match write_result_future.get() {
+        result::Err(e) => return result::Err(e),    // error, so return
+        _ => result::Ok(()),
+    }
+}
+
+/**
+ * Get response from server and parse.
+ * Right now, no parsing; just byte conversion
+ */
+fn get_result(port : @Port<result::Result<~[u8], tcp::TcpErrData>>) -> result::Result<~str, tcp::TcpErrData> {
+    // block until receive message from server over port
+    match port.recv() {
+        result::Err(e) => result::Err(e),
+        result::Ok(response) => result::Ok(str::from_bytes(response)),
+    }
+}
+
+fn connect_to_server_wrap(server_ip_str : ~str, server_port : uint) -> result::Result<(), tcp::TcpErrData> {
+    let server_ip = ip::v4::parse_addr(server_ip_str);
+    let iotask = uv::global_loop::get();
+
+    // connect to server; abort if unable
+    let conn = tcp::connect(server_ip, server_port, &iotask);
+    if result::is_err(&conn) {
+        // rewrap the error: gross
+        return match result::get_err(&conn) {
+            tcp::GenericConnectErr(ename, emsg) => result::Err(tcp::TcpErrData { err_name : ename, err_msg : emsg }),
+            tcp::ConnectionRefused => result::Err(tcp::TcpErrData  { err_name : ~"EHOSTNOTFOUND", err_msg : ~"Invalid IP or port"}),
+        }
+    }
+
+    // extract socket
+    let sock = result::unwrap(conn);
+
+    // begin continuous read; abort if unable
+    let rs = sock.read_start();
+    if result::is_err(&rs) { return result::Err(result::get_err(&rs)); }
+
+    // extract the port over which we read
+    let read_po = result::unwrap(rs);
+    let reader = io::stdin();
+    loop {
+        // get user request
+        print("> ");
+        let line = reader.read_line();
+
+        let req = process_input(copy line);
+
+        // send request to server; abort if able
+        //      in reality, would probably not abort but handle error appropriately
+        let w = sock.write_future(req).get();
+        if result::is_err(&w) { return result::Err(result::get_err(&w)); }
+
+        // if request to disconnect, disconnect
+        if line == ~"q" {
+            sock.read_stop();
+            println("bye!");
+            break;
+        }
+
+        // otherwise, get response (if applicable; here, always applicable)
+        //      in reality might not always want response either
+        let resp = get_result(read_po);
+        if result::is_err(&resp) { return result::Err(result::get_err(&resp)); }
+        println(fmt!("server says: %s", result::unwrap(resp)));
+    }
+
+    // normal exit
+    result::Ok(())
+}
+
+fn main() {
+    let server_ip_str = ~"127.0.0.1";
+    let server_port = 8888u;
+    let iotask = uv::global_loop::get();
+    let reader = io::stdin();
+
+    println("Server will echo everything you say; q to disconnect.");
+    match connect_to_server_wrap(server_ip_str, server_port) {
+        result::Err(e) => println(fmt!("ERR %s : %s", e.err_name, e.err_msg)),
+        _ => (),
+    }
+}
---
+++ tcp_ex/server.rs
@@ -0,0 +1,138 @@
+extern mod core;
+extern mod std;
+
+use core::comm::*;
+use comm = core::comm;
+
+use std::net::*;
+use ip = net_ip;
+use tcp = net_tcp;
+use std::uv;
+
+enum MyResult {
+    MyErr(int),
+    MyQueryResult(~[u8]),
+}
+
+fn on_establish_cb(kill_ch : comm::SharedChan<Option<tcp::TcpErrData>>) {
+    // nothing really
+    println(fmt!("set up server"));
+}
+
+/**
+ * Processes request which comes in bytes. In general, this may consist
+ * of parsing the converted request, carrying out different operations,
+ * depending on the request, etc. Returns either an error flag or a
+ * result (in bytes).
+ *
+ * # Arguments
+ *
+ * * `request` - request in bytes; whatever came from the client
+ *
+ * # Returns
+ *
+ * either a MyError(int) flag
+ * * 0 : disconnect
+ * or the result of the request, in byte format
+ *
+ */
+fn process_request(request : ~[u8]) -> MyResult {
+    let from_client = str::from_bytes(request);
+    match from_client {
+        // if "q", then disconnect normally (exit(0))
+        ~"q" => MyErr(0),
+        // otherwise, the only processing we do here is change the message a bit
+        _ => MyQueryResult(str::to_bytes(fmt!("server did some computation on \"%s\"", from_client))),
+    }
+}
+
+/**
+ * Does main work for establishing new connections, returning early
+ * if there is a error relating to TCP operations, and exiting with
+ * no error otherwise.
+ *
+ * Normally, we would probably not want to fail fatally in so many
+ * of these error cases; for now we do.
+ */
+fn new_connect_cb_single(new_conn : &tcp::TcpNewConnection, kill_ch : &comm::SharedChan<Option<tcp::TcpErrData>>)
+            -> result::Result<(), tcp::TcpErrData> {
+    // accept the connection; abort if unable
+    let a = tcp::accept(*new_conn);
+    if result::is_err(&a) { return result::Err(result::get_err(&a)); }
+
+    // extract the socket from the connection
+    let sock = result::unwrap(a);
+
+    // print info on client that connected
+    let client_addr = sock.get_peer_addr();
+    let client_ip = ip::format_addr(&client_addr);
+    let client_port = ip::get_port(&client_addr);
+    println(fmt!("server connected to %s:%u", client_ip, client_port));
+
+    // begin continuous read; abort if unable
+    let r = sock.read_start();
+    if result::is_err(&r) { return result::Err(result::get_err(&r)); }
+
+    // extract the port over which we read
+    let read_po = result::unwrap(r);
+    loop {
+        // get a request
+        let v = read_po.recv();
+        if result::is_err(&v) { return result::Err(result::get_err(&v)); }
+
+        // process the request
+        let request = result::unwrap(v);
+        match process_request(request) {
+            MyErr(0) => {
+                // exit(0), so stop reading from socket and disconnect
+                sock.read_stop();
+                println(fmt!("server disconnected from %s:%u", client_ip, client_port));
+                break;
+            }
+            MyErr(x) => {
+                // shouldn't get here for now...
+                debug!("ERR unknown error %d!", x);
+            }
+            MyQueryResult(result) => {
+                // write result asynchronously ("future") to the socket; abort if unable
+                let w = sock.write_future(result).get();
+                if result::is_err(&w) { return result::Err(result::get_err(&w)); }
+            }
+        }
+    }
+
+    // normal exit
+    result::Ok(())
+}
+
+fn new_connect_cb(new_conn : tcp::TcpNewConnection, kill_ch : comm::SharedChan<Option<tcp::TcpErrData>>) {
+    // spawn a new task for this connection that will not kill the parent if anything happens to fail
+    do task::spawn_supervised {
+        match new_connect_cb_single(&new_conn, &kill_ch) {
+            result::Err(e) => println(fmt!("ERR %s : %s", e.err_name, e.err_msg)),
+            _ => (),
+        }
+    }
+}
+
+/**
+ * Start server at given IP and port. In general, might also take options
+ * with which to configure server.
+ *
+ * # Arguments
+ *
+ * * `server_ip_str` - server IP, as a string
+ * * `server_port` - server port, as a uint
+ */
+fn start_server(server_ip_str : ~str, server_port : uint) {
+    let server_ip = ip::v4::parse_addr(server_ip_str);
+    let iotask = uv::global_loop::get();
+
+    tcp::listen(server_ip, server_port, 128, &iotask, on_establish_cb, new_connect_cb);
+}
+
+fn main() {
+    println("runs on loop; can connect to several clients at once");
+    // start server with given IP and port, and options [currently none]
+    start_server(~"127.0.0.1", 8888u);
+}
